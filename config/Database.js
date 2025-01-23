267b599f7d55a024e28b6645cd891321e94fc4a7
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2025-01-23 23:12:47 +0530

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = async()=>{
+    console.log(`Connected to backend successfully ${process.env.DATABASE_URL}`)
     mongoose.connect(process.env.DATABASE_URL);
 }
 
